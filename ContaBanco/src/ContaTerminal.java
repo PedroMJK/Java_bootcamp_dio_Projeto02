cb6ac417ea8dc990ec9cca1e1213e0885a7cfcd7
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2024-07-24 14:41:02 -0300

    criado o simulador  de conta

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta!");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        scanner.nextLine(); 
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente!");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo!");
+        double saldo = scanner.nextDouble();
+
+        String mensagem = String.format(
+            "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.",
+            nomeCliente, agencia, numero, saldo
+        );
+
+        System.out.println(mensagem);
+        scanner.close();
     }
 }
